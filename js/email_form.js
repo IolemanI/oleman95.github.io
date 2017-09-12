548fb9f90a9436f95c05e51cb14947049721c2eb
OleMan95 <aom-95@live.com>
2017-09-12 18:24:41 +0300

    Added email form.

--- js/email_form.js
+++ js/email_form.js
@@ -1,16 +1,39 @@
-$(document).ready(function() {
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyCqeEiVOoFE7Genmvw9iSU--XR9VidXa1s",
+	authDomain: "androidtasks-b72a9.firebaseapp.com",
+	databaseURL: "https://androidtasks-b72a9.firebaseio.com",
+	projectId: "androidtasks-b72a9",
+	storageBucket: "androidtasks-b72a9.appspot.com",
+	messagingSenderId: "1075087566120"
+};
+firebase.initializeApp(config);
 
-	$("#form").submit(function() {
-		$.ajax({
-			type: "POST",
-			url: "mail.php",
-			data: $(this).serialize()
-		}).done(function() {
-			$(this).find("input").val("");
-			alert("Еhe Email was sent!");
-			$("#form").trigger("reset");
-		});
-		return false;
-	});
+//on send button click
+function submitClick(){
+  var name = document.getElementById('nameInp').value;
+  var email = document.getElementById('emailInp').value;
+  var message = document.getElementById('msgInp').value;
 
-});
+  writeUserData(name, email, message);
+
+	//display alert 'Message has been sent'
+	document.getElementById('formAlert').style.display = 'block';
+	document.getElementById('email_form').style.height = '320px';
+	setTimeout(function(){
+		document.getElementById('formAlert').style.display = 'none';
+		document.getElementById('email_form').style.height = '265px';
+	}, 3000);
+
+  //clear fields
+  document.getElementById('form').reset();
+}
+
+//write data into firebase database
+function writeUserData(name, email, message) {
+  firebase.database().ref('messages').child('user_' + name).set({
+    name: name,
+    email: email,
+    message: message
+  });
+}
